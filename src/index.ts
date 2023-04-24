8b2f4ca1e8e93cc72e4ac7c8de8d24134275ad3d
DMYTRO TSYHANENKO <DMYTRO.TSYHANENKO@OUTLOOK.COM>
2023-04-24 20:14:23 +0200

    final stage

---
+++ src/index.ts
@@ -0,0 +1,64 @@
+const form = document.getElementById('form') as HTMLFormElement
+const input = document.getElementById('input') as HTMLInputElement
+const todosUL = document.getElementById('todos') as HTMLUListElement
+
+const todos = JSON.parse(localStorage.getItem('todos'))
+
+if(todos) {
+    todos.forEach(todo => addTodo(todo))
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    addTodo()
+})
+
+function addTodo(todo): void {
+    let todoText = input.value
+
+    if(todo) {
+        todoText = todo.text
+    }
+
+    if(todoText) {
+        const todoEl = document.createElement('li')
+        if(todo && todo.completed) {
+            todoEl.classList.add('completed')
+        }
+
+        todoEl.innerText = todoText
+
+        todoEl.addEventListener('click', () => {
+            todoEl.classList.toggle('completed')
+            updateLS()
+        })
+
+        todoEl.addEventListener('contextmenu', (e) => {
+            e.preventDefault()
+
+            todoEl.remove()
+        })
+
+        todosUL.appendChild(todoEl)
+
+        input.value = ''
+
+        updateLS()
+    }
+}
+
+function updateLS() {
+    const todosEl = document.querySelectorAll<HTMLUListElement>('.li')
+
+    const todos = []
+
+    todosEl.forEach(todoEl => {
+        todos.push({
+            text: todoEl.innerText,
+            completed: todoEl.classList.contains('completed')
+        })
+    })
+
+    localStorage.setItem('todos', JSON.stringify(todos))
+}
